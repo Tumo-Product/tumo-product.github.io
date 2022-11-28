25c5c529ea5a5f33684f94078aeca591ef199983
Mark Amiraghyan <mark.amiraghyan@tumo.org>
2022-11-28 18:16:27 +0400

    feat: add animation blackbox

---
+++ js/functions.js
@@ -0,0 +1,134 @@
+function copyToClipboard(str) { // Copy a string to the clipboard
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+
+function playAnimation() {
+    background('#f3f3f3') // The background image
+    _drawFrame(lastPlayedFrame, 255, _ballColor); // Draw the currently played frame with alpha of 255 (100 %)
+    if (lastPlayedFrame != _frameCount - 1 && frames[lastPlayedFrame + 1].edited) { // Continue if there are frames left
+        return lastPlayedFrame++; // Continue to the next frame
+    }
+    return lastPlayedFrame = 0; // If frames ended, restart
+}
+
+function drawFrames(index) {
+    // Clear the screen
+    background('#f3f3f3');
+
+    // Draw previous frames
+    let _alpha = _previouseFrameAlphaFirstValue / 100 * 255;
+    for (let i = 0; i < _previouseFrameCount && i < currentFrame; i++) {
+        _drawFrame(index - i - 1, _alpha - (i * (_previouseFrameAlphaDiff / 100 * 255)), _previousFramesColor); // Looks scary, isn't it ?
+    }
+
+    if (showNextFrames) {
+        // Draw next frames
+        for (let i = 0; i < _nextFrameCount && index + i + 1 < _frameCount; i++) {
+            _drawFrame(index + i + 1, _alpha - (i * (_nextFrameAlphaDiff / 100 * 255)), _nextFramesColor);
+        }
+    }
+
+    // Draw the current frame
+    _drawFrame(index, 255, _ballColor);
+}
+
+function _drawFrame(index, _alpha, _color) { // Draws a single frame with the given alpha
+    const { x, y } = frames[index];
+    fill(..._color, _alpha);
+    ellipse(x, y, _ballSize, _ballSize);
+}
+
+function nextFrame() { // Switches to the next frame (in edit mode)
+    if (currentFrame < frames.length - 1) {
+        currentFrame++;
+    }
+}
+
+function previousFrame() { // Switches to the previous frame (in edit mode)
+    if (currentFrame > 0) {
+        currentFrame--;
+    }
+}
+
+function setupFrames() { // Setup the  frames
+    isAnimationPlaying ? Play() : null; // Stop animation if it is playing
+    lastPlayedFrame = 0; // Reset the animation play frame
+    frames = []; // Empty the frames
+    currentFrame = 0; // Reset the edit mode frame
+    for (let i = 0; i < _frameCount; i++) { // Filling the frames with default positioned balls
+        frames.push({ x: _defaultBallX, y: _defaultBallY, edited: false });
+    }
+}
+
+function Play() { // Play or pause the animation
+    const div = document.getElementById('play'); // The "Play" button
+    if (isAnimationPlaying) { // If animation is playing, stop
+        div.classList.remove('pause'); // Reset the button image
+        lastPlayedFrame = 0; // Reset the animation play frame
+        return isAnimationPlaying = false; // Stop animation
+    }
+    // If animation is not playing, start
+    div.classList.add('pause'); // Change the 'Play' button to the 'Pause' button
+    return isAnimationPlaying = true; // Start the animation
+}
+
+function Save() { // Save the animation
+    let activeFramesCount = 0;
+    for (let i in frames) if (frames[i].edited) activeFramesCount++;
+    if (activeFramesCount >= 15) {
+        if (confirm("Copy frames to clipboard ?")) {
+            copyToClipboard(JSON.stringify(frames));
+        }
+
+    }
+    else alert("Too few frames, you need to create 15 frames minimum");
+}
+
+function Import() { // Import an animation
+    // Prompt for the animation
+    const res = prompt("Insert your data here");
+    try {
+        if (!res) throw new Error('This error will be catched, baby.')
+        frames = JSON.parse(res); // Parse the data
+        isAnimationPlaying ? Play() : null; // Stop animation if it is playing
+        currentFrame = 0; // Go to frame 0
+    }
+    catch (e) { // Wrong data was inserted
+        alert("Something went wrong, try again !");
+    }
+}
+
+function drawProgressBar(frame, animate) { // Draw the progress bar with the given frame and animate? it
+    stroke('green'); // Line color
+    strokeWeight(10); // Weight is 3px
+
+    // Animate the progress bar using linear interpolation or just set it's value immediately
+    progressBarWidth = animate ? lerp(progressBarWidth, progressBarStepWidth * (frame), 0.2) : progressBarStepWidth * (frame);
+
+    // Draw the progress bar
+    line(0, 0, progressBarWidth, 0);
+
+    // Displaying the frame number
+    strokeWeight(4);
+    fill(..._nextFramesColor);
+    textSize(48);
+    text(frame + 1, 12, 54);
+
+    // Disable stroke
+    return noStroke();
+}
+
+function nextFrames() {
+    const div = document.getElementById('nextframes');
+    if (showNextFrames) {
+        showNextFrames = false;
+        return div.classList.remove('nonextframes');
+    }
+    showNextFrames = true;
+    div.classList.add('nonextframes')
+}
---
+++ js/sketch.js
@@ -0,0 +1,96 @@
+/**
+ * @description The black box for the franchise animation activity
+ * @author Mark Amiraghyan 
+ */
+
+// All constants that are starting with '_' are config variables
+const _width = 640, _height = 640; // Canvas size
+const _ballSize = 80; // Size of the ball (Ball radius times 2)
+const _ballColor = [36, 36, 36]; // Color of the ball [R, G, B]
+const _previousFramesColor = [229, 57, 53];
+const _nextFramesColor = [139, 195, 74];
+const _frameCount = 24; // Count of animation frames
+const _defaultBallX = _width / 2, _defaultBallY = _height / 2;
+const _previouseFrameCount = 5; // Count for seen previous frames
+const _nextFrameCount = 5; // Count for seen next frames
+const _previouseFrameAlphaFirstValue = 70; // Alpha value for the first previous frame (in percents)
+const _previouseFrameAlphaDiff = 15; // Alpha differance
+const _nextFrameAlphaDiff = 8; // Alpha differance
+const _defaultFrameRate = 18; // Default frame rate for animation
+const _frameRateSliderWidth = _width / 2; // Width of the frame rate slider
+
+const progressBarStepWidth = _width / (_frameCount - 1); // Progress bar's step width
+
+let frames = []; // Create the frames
+let currentFrame = 0; // Current active frame
+let isDragging = false; // Is mouse dragging the ball now ?
+let Canvas; // The p5 Canvas element
+let progressBarWidth = 0; // Progress bar's width
+let isAnimationPlaying = false; // Is Animation playing ?
+let lastPlayedFrame = 0; // Last frame played
+let FrameRate; // The frame rate slider
+let FrameRateP; // The frame rate paragraph
+let showNextFrames = true; // Show next frames or not
+
+function preload() {
+
+}
+
+function setup() {
+    Canvas = createCanvas(_width, _height); // Create the canvas with size of _width and _height
+    Canvas.parent('animationCanvas'); // Setting the location of the canvas
+
+    background('#f3f3f3'); // Fill the background image
+    noStroke(); // We don't need stroke here
+
+    FrameRateP = document.getElementById('slider-fps');
+
+    FrameRate = createSlider(1, 60, _defaultFrameRate); // Create the FPS slider
+    FrameRate.parent('slider'); // Place it in the <div id = "slider">
+    FrameRate.style('width', `${_frameRateSliderWidth}px`) // Configure the width
+
+    setupFrames(); // Setup the frames
+}
+
+function draw() {
+    FrameRateP.innerText = `Animation Speed: ${FrameRate.value()} FPS`;
+    if (!isAnimationPlaying) { // Edit mode
+        frameRate(60); // FPS is always 60 in the edit mode
+        drawFrames(currentFrame); // Draw the frames
+        return drawProgressBar(currentFrame, true); // Draw the progress bar for the current frame and animate it
+    }
+    // Play mode
+    frameRate(FrameRate.value()); // Take the FPS value fomr the slider
+    playAnimation(); // Play the animation
+    drawProgressBar(lastPlayedFrame, false); // Draw the progress bar without animation
+}
+
+function mouseDragged() { // Works when mouse is dragged
+    if (!isAnimationPlaying) { // User can't edit anything when animation is playing
+        const { x, y } = frames[currentFrame]; // Current ball's X and Y coordinates
+        if ((dist(mouseX, mouseY, x, y) <= (_ballSize / 2)) || isDragging) { // if user is dragging the ball
+            isDragging = true; // Make sure user is still dragging
+            background('#f3f3f3'); // Clear the background
+            frames[currentFrame].x += mouseX - pmouseX; // Change ball's X position
+            frames[currentFrame].y += mouseY - pmouseY; // Change ball's Y postiion
+            frames[currentFrame].edited = true; // Activate the frame
+        }
+    }
+}
+
+function mouseReleased() { // Works when mouse is released
+    isDragging = false; // User is not dragging anymore
+}
+
+function keyPressed() { // Works when a key is pressed
+    if (!isAnimationPlaying) {
+        switch (keyCode) { // Switch the frame to right or to left
+            case RIGHT_ARROW:
+                nextFrame(); // Switch to the next frame
+                break;
+            case LEFT_ARROW:
+                previousFrame(); // Switch to the previous frame
+                break;
+        }
+    }
+}
